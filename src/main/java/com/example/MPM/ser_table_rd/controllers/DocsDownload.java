3942e59f70df6dbbc9786ba033b0271f8e39e505
NikitaOomory <hackyourw@gmail.com>
2023-02-08 22:52:45 +0300

    Добавлена тестовая версия скачивания документа

--- src/main/java/com/example/MPM/ser_table_rd/controllers/DocsDownload.java
+++ src/main/java/com/example/MPM/ser_table_rd/controllers/DocsDownload.java
@@ -4,8 +4,18 @@ import com.example.MPM.contract.MyPagePath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/tableRD/download")
@@ -14,7 +24,21 @@ public class DocsDownload {
     MyPagePath path = new MyPagePath();
 
     @GetMapping
-    public String getPage(){
+    public String getPage() {
         return path.REG_DOCS;
+    }
+
+    @GetMapping("/1")
+    public void getFile(HttpServletResponse response) throws IOException {
+        File file = new File("text.txt");
+        if (file.exists()){
+            response.setHeader("Content-disposition", "attachment;filename=" + file.getName());
+            response.setContentType("application/vnd.ms-excel");
+
+                Files.copy(file.toPath(), response.getOutputStream());
+                response.getOutputStream().flush();
+
+            //TODO: изучить и отредактировать!!!
+        }
     }
 }
